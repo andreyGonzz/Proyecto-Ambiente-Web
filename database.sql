c83a68f179729e18024c1eed053fc2205db5cf11
Jorge Andrey Gonzalez Ruiz <jorgeandreygonzalez2006@gmail.com>
2026-07-31 16:44:44 -0600

    bd

--- database.sql
+++ database.sql
@@ -5,5 +5,6 @@ create table if not exists usuarios(
 id int auto_increment primary key,
 nombre varchar(250),
 correo varchar(250),
-contrasena varchar(250)
+contrasena varchar(250),
+rol varchar(250)
 );
