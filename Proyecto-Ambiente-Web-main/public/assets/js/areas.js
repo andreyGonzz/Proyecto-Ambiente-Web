8ecdb19e69a93db7f3c2a6ff735af7efe13918cd
Jorge Andrey Gonzalez Ruiz <jorgeandreygonzalez2006@gmail.com>
2026-08-09 14:37:15 -0600

    Correccion de no se

--- Proyecto-Ambiente-Web-main/public/assets/js/areas.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('.relleno').forEach((bar) => {
-        const target = `${bar.getAttribute('data-target')}%`;
-        setTimeout(() => {
-            bar.style.width = target;
-        }, 300);
-    });
-});
--- Proyecto-Ambiente-Web-main/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '../app/core/App.php';
-require_once '../app/core/Controller.php';
-require_once '../app/config/config.php';
-
-// Iniciar la aplicación
-$app = new App();
